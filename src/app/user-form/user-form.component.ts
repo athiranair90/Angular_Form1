a2d83c3776e082b3393138be76e74be4acafc0b6
athiranair90 <50161056+athiranair90@users.noreply.github.com>
2021-04-04 10:57:49 +0530

    Add files via upload

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, Form, NgForm } from '@angular/forms';
+import { User } from './file1';
+
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+  showList: boolean = false;
+  firstName: string = "Coder";
+  lastName: string = "Byte";
+  Phone: Number = 8885559999;
+  defaultuser: any;
+  userList: Array<User> = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    //this.firstName
+    let defaultuser = new User();
+    defaultuser.firstName = "Coder";
+    defaultuser.lastName = "Byte";
+    defaultuser.Phone = 8885559999;
+
+    //this.userList.push(defaultuser)
+  }
+
+  AddUser(data: NgForm) {
+    console.log(data.value)
+    let user = new User();
+    user.firstName = data.value.fname;
+    user.lastName = data.value.lname;
+    user.Phone = Number(data.value.mob);
+
+    if (this.userList.some(a => a.Phone === data.value.mob)) {
+      this.showList = true;
+    } else {
+      this.showList = false;
+      this.userList.push(user);
+    }
+
+    this.userList.sort(function (a, b) {
+      return a.lastName.localeCompare(b.lastName);
+    });
+
+    console.log(this.userList.length);
+    console.log(data.value.mob);
+  }
+
+}
